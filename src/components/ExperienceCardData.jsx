b5e516befee1d2d383cf6b7db9bc5b53ad551667
Vikas Kumar Thakur <40897867+vkskumarthakur@users.noreply.github.com>
2024-07-09 06:46:56 +0000

    addan experiece card added to experiences section and whatsApp connection added in connections

--- src/components/ExperienceCardData.jsx
+++ src/components/ExperienceCardData.jsx
@@ -1,29 +1,38 @@
 const experiencedata = [
   {
     id: 1,
-    imgsrc: "https://www.mindrops.com/images/logo.png",
-    companyName: "Mindrops Solutions Pvt LTd   ",
-    startDate: "Dec 2022",
+    imgsrc: "https://www.adaan.com/wp-content/uploads/2022/11/logo3.svg",
+    companyName: "Adaan Digital Solutions Pvt LTd",
+    startDate: "May 2024",
     endDate: "Present",
-    role: "Software Engineer",
+    role: "Front-End Developer",
   },
   {
     id: 2,
-    imgsrc:
-      "https://ci3.googleusercontent.com/meips/ADKq_NYMIzmZ2VjiiWp28knR6J-3PGIin5hHe8OFpcFZuuYdvpVSV2ftPuP0SFeT-QmjSSHRafU0tesupND7YJXrbl4r9lA_cWxwSGgi0u4X=s0-d-e1-ft#https://admin.lazymonkey.in/new_admin/dist/img/logo.png",
-    companyName: "LazyMonkey",
+    imgsrc: "https://www.mindrops.com/images/logo.png",
+    companyName: "Mindrops Solutions Pvt LTd   ",
     startDate: "Dec 2022",
-    endDate: "Present",
+    endDate: "Apr 2024",
     role: "Software Engineer",
   },
   {
     id: 3,
+    imgsrc:
+      "https://ci3.googleusercontent.com/meips/ADKq_NYMIzmZ2VjiiWp28knR6J-3PGIin5hHe8OFpcFZuuYdvpVSV2ftPuP0SFeT-QmjSSHRafU0tesupND7YJXrbl4r9lA_cWxwSGgi0u4X=s0-d-e1-ft#https://admin.lazymonkey.in/new_admin/dist/img/logo.png",
+    companyName: "LazyMonkey",
+    startDate: "Dec 2022",
+    endDate: "Apr 2024",
+    role: "Software Engineer",
+  },
+  {
+    id: 4,
     imgsrc: "https://i.ytimg.com/vi/dgtQD0ecF4o/maxresdefault.jpg",
     companyName: "EdiQue Solutions Private Limited",
     startDate: "Nov 2019",
     endDate: "Aug 2022",
     role: "ICT Instructor",
   },
+  
 ];
 
 export default experiencedata;
